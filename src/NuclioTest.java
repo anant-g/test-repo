2e5149dbb2925e5fe1b2504df7bd383a58867a5e
anantg <anant1985@gmail.com>
2019-07-31 11:29:18 +0800

    removed uber jar

---
+++ src/NuclioTest.java
@@ -0,0 +1,14 @@
+import io.nuclio.Context;
+import io.nuclio.Event;
+import io.nuclio.EventHandler;
+import io.nuclio.Response;
+
+public class NuclioTest implements EventHandler {
+
+	public Response handleEvent(Context context, Event event) {
+		context.getLogger().infoWith("in nuclio function", "body", "in function");
+
+		HelloWorld helloWorld = new HelloWorld();
+		return new Response().setBody(helloWorld.testCall());
+	}
+}
